eacfef0b3507f6dfcf65e5240c61e76dca3b9182
Andrew Ngo <ngoandrew00@gmail.com>
2021-02-18 13:08:11 -0800

    Styling done

--- projectcode/scripts/timer.js
+++ projectcode/scripts/timer.js
@@ -1,14 +1,15 @@
-
-var minutes = 25;
-var seconds = 0;
-var interval;
-
+let minutes;
+let seconds;
+let interval;
+let pomos;
 
 function timeStart() {
-    if (document.getElementById("startButton").textContent == "Start") {
+    if (document.getElementById("startButton").textContent == "START") {
+        minutes = 25;
+        seconds = 0;
         interval = setInterval(count, 1000);
         document.getElementById("clock").innerHTML = "25:00";
-        document.getElementById("startButton").textContent = "Stop";
+        document.getElementById("startButton").textContent = "STOP";
     } else {
         stop();
     }
@@ -16,32 +17,54 @@ function timeStart() {
 
 function count() {
     seconds--;
-    if(seconds == -1){
-        
+    if (seconds == -1) {
         minutes--;
         
         if(minutes == -1){
             clearInterval(interval);
-        }
-        else{
+            switchTimes();
+        } else {
             seconds = 59;
         }
     }
-    if(seconds >= 10){
+    if (seconds >= 10) {
         document.getElementById("clock").innerHTML = minutes + ":" + seconds;
-    }   
-    else{
+    } else {
         document.getElementById("clock").innerHTML = minutes + ":0" + seconds;
     } 
 }
 
-function stop() {
-    if (confirm("This will stop the timer and reset all pomodoro breaks. Are you sure you want to conitnue?")) {
-        clearInterval(interval);
+function switchTimes() {
+    if (document.getElementById("state").textContent == "Work") {
+    pomos++;
+        document.getElementById("state").textContent = "Short Break";
+        minutes = 5;
         seconds = 0;
+        interval = setInterval(count, 1000);
+        document.getElementById("clock").innerHTML = "5:00";
+    } else if (document.getElementById("state").textContent == "Short Break") {
+        document.getElementById("state").textContent = "Long Break";
+        minutes = 30;
+        seconds = 0;
+        interval = setInterval(count, 1000);
+        document.getElementById("clock").innerHTML = "30:00";
+    } else if (document.getElementById("state").textContent == "Long Break") {
+        document.getElementById("state").textContent = "Work";
         minutes = 25;
+        seconds = 0;
+        interval = setInterval(count, 1000);
         document.getElementById("clock").innerHTML = "25:00";
-        document.getElementById("startButton").textContent = "Start";
+    }
+}
+
+function stop() {
+    if (confirm("This will stop the timer and reset all Pomodoro breaks. Are you sure you want to continue?")) {
+        clearInterval(interval);
+        minutes = 25;
+        seconds = 0;
+        document.getElementById("clock").innerHTML = "25:00";
+        document.getElementById("startButton").textContent = "START";
+        document.getElementById("state").textContent = "Work";
     } else {
         alert("The timer will continue!");
     }
