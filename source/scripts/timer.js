67c2fb50fca57ae1e963263abb1573474be01d9e
nathanbui90 <58157086+nathanbui90@users.noreply.github.com>
2021-03-06 18:02:38 -0800

    Feature/notifications (#34)
    * added notifications
    
    * fixed notification sound timing and edited work periods completed counter to include the last pomo
    
    * added desktop notifications and fixed the createTask function because it did not properly handle invalid numbers which are less than 1
    
    * Delete source directory
    
    * Rename projectcode/scripts/menu.js to source/scripts/menu.js
    
    * Rename source/scripts/menu.js to projectcode/scripts/menu.js
    
    * Rename Projectcode to Source
    
    Co-authored-by: bewjai <billwu1012@gmail.com>
    Co-authored-by: nathan42424 <nathanbui90@gmail.com>

--- source/scripts/timer.js
+++ source/scripts/timer.js
@@ -1,34 +1,36 @@
 let minutes;
 let seconds;
 let interval;
+let interval2;
 let pomos = 0;
 let longBreakCounter = 0;
 var currPomos = 0;
 var currTask = document.getElementById('currentTask');
-let settingsInput = document.getElementById("settingsInput");
-let overlay = document.getElementById("overlay");
-
+var notification;
+var endOfEstimated = false;
 /**
  * Starts the timer when start button is clicked
  * and lets user know if they don't have tasks to do.
  */
-function timeStart() {
+function timeStart() { 
     if (document.getElementById("startButton").textContent == "START") {
         if(document.getElementById('taskList').firstChild == null){
             alert('There are no tasks to do!');
             return;
         }
         moveTask();
-
         minutes = 25;
         seconds = 0;
         interval = setInterval(count, 1000);
+        interval2 = setInterval(notifications, 1000);
         document.getElementById("clock").innerHTML = "25:00";
+
         document.getElementById("startButton").textContent = "STOP";
     } else {
         stop();
     }
 }
+
 
 /**
  * Timer to countdown every second based off the default
@@ -41,6 +43,8 @@ function count() {
         
         if(minutes == -1){
             clearInterval(interval);
+            clearInterval(interval2);
+
             if(document.getElementById("state").textContent == "Work"){
                 
                 taskTracker();
@@ -52,8 +56,7 @@ function count() {
             }
             
             switchTimes();
-        
-        } 
+        }
         else {
             seconds = 59;
         }
@@ -64,31 +67,36 @@ function count() {
         document.getElementById("clock").innerHTML = minutes + ":0" + seconds;
     } 
 }
-
-/**
- * Moves the task
- */
+function taskTracker(){
+    currPomos--;
+    if(currPomos == 0){
+        /*
+        notification = new Notification("Task's Estimated Pomos Over", {
+            body: "Good job! The current task's estimated pomos are over. \nPlease return to the website to input whether you want to add more pomos to this task.",
+        });
+        */
+        addTime();
+    }
+    if(currPomos == 0 && document.getElementById('taskList').firstChild != null){
+        moveTask();
+    }
+}
+function addTime(){
+    currPomos = prompt("The estimated pomos are up. \nHow many work periods would you like to add? \nPlease input a number:");
+    while(isNaN(currPomos)){
+        currPomos = prompt("You have not entered a number. \nPlease re-enter a number of work periods you would like to add:");
+    }
+    if(currPomos == null || currPomos  == "" || currPomos == 0){
+        currPomos = 0;
+        alert("You have chosen not to add additional pomos to this task");
+    }
+}
 function moveTask(){
     currPomos = parseInt(document.getElementById('taskList').firstChild.getAttribute('taskPomos'));
     currTask.innerHTML = document.getElementById('taskList').firstChild.getAttribute('taskName');
     document.getElementById('taskList').removeChild(document.getElementById('taskList').firstChild);
 }
 
-/**
- * Keeps track of the current task and the work periods completed.
- */
-function taskTracker(){
-        currPomos--;
-    if(currPomos == 0 && document.getElementById('taskList').firstChild != null){
-        moveTask();
-    }
-}
-
-/**
- * Switches the current task from work to short break and
- * long break depending on the amount of pomodoros they've
- * completed.
- */
 function switchTimes() {
     if (document.getElementById("state").textContent == "Work") {
         // Increment total pomo counter and update page
@@ -104,6 +112,7 @@ function switchTimes() {
             minutes = 5;
             seconds = 0;
             interval = setInterval(count, 1000);
+            interval2 = setInterval(notifications, 1000);
             document.getElementById("clock").innerHTML = "05:00";
         } else { // Take a long break
             longBreakCounter = 0;
@@ -111,7 +120,9 @@ function switchTimes() {
             minutes = 30;
             seconds = 0;
             interval = setInterval(count, 1000);
+            interval2 = setInterval(notifications, 1000);
             document.getElementById("clock").innerHTML = "30:00";
+
         }
     // After break, get back to work
     } else if (document.getElementById("state").textContent == "Short Break" 
@@ -120,18 +131,23 @@ function switchTimes() {
         minutes = 25;
         seconds = 0;
         interval = setInterval(count, 1000);
+        interval2 = setInterval(notifications, 1000);
+
         document.getElementById("clock").innerHTML = "25:00";
     } 
 }
 
-/**
- * Stops the timer and will hard reset all the tasks.
- */
 function stop() {
     if(document.getElementById('taskList').firstChild == null){
+        pomos++;
+        document.getElementById("workPeriods").innerHTML = pomos;
+        notification = new Notification("No more tasks", {
+            body: "Congratulations! You have finished all your tasks! \nPlease input more tasks if needed.\nIf not, thank you for using our service!",
+        });
+        setTimeout(notification.close(), 1 * 1000);
         alert('No tasks left to do!');
         clearInterval(interval);
-        minutes = 1;
+        minutes = 25;
         seconds = 0;
         document.getElementById("clock").innerHTML = "25:00";
         document.getElementById("startButton").textContent = "START";
@@ -139,7 +155,7 @@ function stop() {
     }
     else if (confirm("This will stop the timer and reset all Pomodoro breaks. Are you sure you want to continue?")) {
         clearInterval(interval);
-        minutes = 1;
+        minutes = 25;
         seconds = 0;
         document.getElementById("clock").innerHTML = "25:00";
         document.getElementById("startButton").textContent = "START";
@@ -150,17 +166,80 @@ function stop() {
     }
 }
 
-/**
- * Displays the settings page.
- */
+function notificationPermission(){
+    if (!window.Notification) {
+        alert("Browser does not support notifications");
+    }
+    else {
+        if(Notification.permission === 'granted'){
+            //alert("granted"); 
+        }
+        else if (Notification.permission !== 'denied'){
+            //alert("not granted");
+            Notification.requestPermission().then(function (p) {
+                if(p === 'granted'){
+                }
+            });
+        }
+    }
+}
+function popupNotification() {
+    
+    if(currPomos == 1 && document.getElementById("state").textContent == "Work"){
+        notification = new Notification("Task's Estimated Pomos Over", {
+            body: "Good job! The current task's estimated pomos are over. \nPlease return to the website to input whether you want to add more pomos to this task.",
+        });
+    }
+    else if (document.getElementById("state").textContent == "Work" && currPomos > 1){
+        if(longBreakCounter < 3){
+            notification = new Notification("Work Period Over", {
+                body: "Good job on the work so far! \nHere is a short break.",
+            });
+        }
+        else{
+            notification = new Notification("Work Period Over", {
+                body: "Good job staying on task! \nHere is a well-deserved long break.",
+            });
+        }
+    }
+    else if (document.getElementById("state").textContent == "Short Break"){
+        notification = new Notification("Short Break Over", {
+            body: "Your break time is up \n You should resume working",
+        });
+    }
+    else if (document.getElementById("state").textContent == "Long Break"){
+        notification = new Notification("Long Break Over", {
+            body: "Your break time is up \n You should resume working",
+        });
+    }
+    setTimeout(notification.close(), 1 * 1000);
+
+}
+function soundNotification(){
+    var audio = new Audio("../sounds/samsung_whistle.mp3");
+    audio.play();
+}
+function notifications(){
+    if(seconds == 0 && minutes == 0){
+        popupNotification();
+        soundNotification();
+    }
+}
+
+
+let settingsInput = document.getElementById("settingsInput");
+let overlay = document.getElementById("overlay");
+
 function displaySettings() {
     settingsInput.style.display = "block";
     overlay.style.display = "block";
 }
 
+
 /**
  * Closes the settings page.
  */
+
 function settingsClose() {
     settingsInput.style.display = "none";
     overlay.style.display = "none";
