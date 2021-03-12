b86bc12345baebe97ff331b9fdb32a6b1db30bc3
bamkho <tmphan@ucsd.edu>
2021-03-12 14:46:37 -0800

    Added language feature

--- source/scripts/taskAdd.js
+++ source/scripts/taskAdd.js
@@ -8,21 +8,27 @@ var numberOfTasks = 0;
  * of the task as well as the expected pomos it 
  * would take.
  */
-function displayInput(){
-    if(taskInput.style.display == "none"){
+function displayInput() {
+    if (taskInput.style.display == "none") {
         taskInput.style.display = "block";
     }
-    else{
+    else {
         taskInput.style.display = "none";
     }
+}
+
+var positiveNumberText = {
+    en: "Please re-enter a positive number for the number of expected pomos.",
+    es: "Vuelva a ingresar un número positivo para el número de pomos esperados.",
+    ch: "請重新輸入一個正數，以表示預期的pomos數量。”"
 }
 
 /**
  * Creates the task and adds it to the task list.
  */
-function createTask(){
-    if(taskPomos.value < 1){
-        alert("Please re-enter a positive number for the number of expected pomos.");
+function createTask() {
+    if (taskPomos.value < 1) {
+        alert(positiveNumberText[localStorage.getItem("language")]);
         return;
     }
     numberOfTasks++;
@@ -35,79 +41,91 @@ function createTask(){
  * 
  * @param {event} event The event related to the task that will be deleted. 
  */
-function deleteTask(event){
+function deleteTask(event) {
     let orderedList = document.getElementById("taskList").children;
     let current = event.target.parentNode.children;
     let i = 0;
-    while(current[0].innerHTML != orderedList[i].shadowRoot.querySelector(".name").innerHTML){
+    while (current[0].innerHTML != orderedList[i].shadowRoot.querySelector(".name").innerHTML) {
         i++;
     }
     document.getElementById("taskList").removeChild(orderedList[i]);
+}
+
+var taskAtTopText = {
+    en: "The task is at the top already.",
+    es: "La tarea ya está en la cima.",
+    ch: "任務已經是最重要的。"
 }
 
 /**
  * Swap the current task with the task above it. If there is no task above the current then
  * alert the user and return nothing.
  */
-function upTask(event){
+function upTask(event) {
     let orderedList = document.getElementById("taskList").children;
     let current = event.target.parentNode.children;
     let temp;
     let tempNum;
     let i = 0;
-    
-    while(current[0].innerHTML != orderedList[i].shadowRoot.querySelector(".name").innerHTML){
+
+    while (current[0].innerHTML != orderedList[i].shadowRoot.querySelector(".name").innerHTML) {
         i++;
     }
 
-    if(i == 0){
-        alert("The task is at the top already.");
+    if (i == 0) {
+        alert(taskAtTopText[localStorage.getItem("language")]);
         return;
     }
-    else{
+    else {
         temp = current[0].innerHTML;
         tempNum = current[1].innerHTML;
-        current[0].innerHTML = orderedList[i-1].shadowRoot.querySelector(".name").innerHTML;
-        current[1].innerHTML = orderedList[i-1].shadowRoot.querySelector(".numPomos").innerHTML;
+        current[0].innerHTML = orderedList[i - 1].shadowRoot.querySelector(".name").innerHTML;
+        current[1].innerHTML = orderedList[i - 1].shadowRoot.querySelector(".numPomos").innerHTML;
         orderedList[i].setAttribute("taskName", current[0].innerHTML);
         orderedList[i].setAttribute("taskPomos", current[1].innerHTML);
-        orderedList[i-1].setAttribute("taskName", temp);
-        orderedList[i-1].setAttribute("taskPomos", tempNum);
-        orderedList[i-1].shadowRoot.querySelector(".name").innerHTML = temp;
-        orderedList[i-1].shadowRoot.querySelector(".numPomos").innerHTML = tempNum;
+        orderedList[i - 1].setAttribute("taskName", temp);
+        orderedList[i - 1].setAttribute("taskPomos", tempNum);
+        orderedList[i - 1].shadowRoot.querySelector(".name").innerHTML = temp;
+        orderedList[i - 1].shadowRoot.querySelector(".numPomos").innerHTML = tempNum;
     }
+}
+
+var taskAtBottomText = {
+    en: "The task is at the bottom already.",
+    es: "La tarea ya está al final.",
+    ch: "任務已經在底部了."
 }
 
 /**
  * Swap the current task with the task below it. If there is no task below the current then
  * alert the user and return nothing.
  */
-function downTask(event){
+function downTask(event) {
     let orderedList = document.getElementById("taskList").children;
     let current = event.target.parentNode.children;
     let temp;
     let tempNum;
     let i = 0;
-    
-    while(current[0].innerHTML != orderedList[i].shadowRoot.querySelector(".name").innerHTML){
+
+    while (current[0].innerHTML != orderedList[i].shadowRoot.querySelector(".name").innerHTML) {
         i++;
     }
 
-    if(orderedList[i+1] == null){
-        alert("The task is at the bottom already.");
+    if (orderedList[i + 1] == null) {
+        alert(taskAtBottomText[localStorage.get("language")]);
         return;
     }
-    else{
+    else {
         temp = current[0].innerHTML;
         tempNum = current[1].innerHTML;
-        current[0].innerHTML = orderedList[i+1].shadowRoot.querySelector(".name").innerHTML;
-        current[1].innerHTML = orderedList[i+1].shadowRoot.querySelector(".numPomos").innerHTML;
+        current[0].innerHTML = orderedList[i + 1].shadowRoot.querySelector(".name").innerHTML;
+        current[1].innerHTML = orderedList[i + 1].shadowRoot.querySelector(".numPomos").innerHTML;
         orderedList[i].setAttribute("taskName", current[0].innerHTML);
         orderedList[i].setAttribute("taskPomos", current[1].innerHTML);
-        orderedList[i+1].setAttribute("taskName", temp);
-        orderedList[i+1].setAttribute("taskPomos", tempNum);
-        orderedList[i+1].shadowRoot.querySelector(".name").innerHTML = temp;
-        orderedList[i+1].shadowRoot.querySelector(".numPomos").innerHTML = tempNum;
+        orderedList[i + 1].setAttribute("taskName", temp);
+        orderedList[i + 1].setAttribute("taskPomos", tempNum);
+        orderedList[i + 1].shadowRoot.querySelector(".name").innerHTML = temp;
+        orderedList[i + 1].shadowRoot.querySelector(".numPomos").innerHTML = tempNum;
     }
 }
 
