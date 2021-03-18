71f8461e186c91d8eae73cd14ceb3ac8c3433e43
ngoandrew <53445172+ngoandrew@users.noreply.github.com>
2021-03-18 20:19:34 +0000

    Deploying to master from @ elliscchang/cse110-w21-group8@fb30787c2c9a4b6c4aedc72c5bfdcbdda921df4e 🚀

--- cypress/integration/tests.js
+++
@@ -1,9 +0,0 @@
-describe('Tests for Pomodoro Timer', () => {
-    beforeEach(() => {
-        cy.visit('http://127.0.0.1:5500/');
-    });
-    
-    it('First Test', () => {
-        expect(true).to.equal(true);
-    });
-})
