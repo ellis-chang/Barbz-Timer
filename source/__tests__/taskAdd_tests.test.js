1eb3f9d6abc3bf477816ac8a49f21344f3e35ea2
elliscchang <ecc013@ucsd.edu>
2021-03-18 00:28:05 -0700

    added tests for menu and taskAdd js files

---
+++ source/__tests__/taskAdd_tests.test.js
@@ -0,0 +1,49 @@
+const {displayInput, createTask, deleteTask, upTask, downTask} = require('../scripts/taskAdd');
+
+document.body.innerHTML = `
+    <div id="taskInput" style="display: none;"></div>
+    <ol id="taskList"></ol>
+    <button id="addTask" onclick="displayInput()">Add a task</button>
+            <div>
+                <br>
+                <div id="taskInput" style="display: none;">
+                    <label id="taskNameLang" for="taskName">Task Name: </label>
+                    <input type="taskName" name="taskName" id="taskName">
+                    <br>
+                    <br>
+                    <label id="expectedPomosLang" for="taskPomos">Number of Expected Pomos: </label>
+                    <input type="number" name="taskPomos" id="taskPomos" min="1" max="99">
+                    <br>
+                    <br>
+                    <button id="createTask" onclick="createTask()">Create</button>
+                </div>
+            </div>
+`;
+
+describe('Input form is ', () => {
+    test('displayed', () => {
+        displayInput();
+        expect(document.getElementById("taskInput").style.display).toEqual("block");
+    })
+
+    test('hidden', () => {
+        displayInput();
+        expect(document.getElementById("taskInput").style.display).toEqual("none");
+    })
+})
+
+describe('Creating a task ', () => {
+    test('works', () => {
+        //const alert = jest.fn();
+        createTask();
+        expect(document.getElementById("taskList").children.length).toBe(0);
+        //expect(alert).toBeCalledTimes(1);
+    })
+})
+
+/*describe('Deleting a task ', () => {
+    test('works', () => {
+        deleteTask(event);
+        expect(document.getElementById("taskList").children.length).toBe(0);
+    })
+})*/
